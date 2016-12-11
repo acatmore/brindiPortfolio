dbd3e2bf925b691a2c06278ad11ed2cba6a9d755
Alex Atmore <acatmore@gmail.com>
2016-12-11 13:15:54 -0800

    setup initial test

--- js/index.jsx
+++ js/index.jsx
@@ -3,13 +3,12 @@ var React      = require('react');
 var ReactDOM   = require('react-dom');
 //var Provider = require('react-redux').Provider;
 //var store    = require('./store');
+var App        = require('.components/App');
 
 
 document.addEventListener('DOMContentLoaded', function() {
 	ReactDOM.render(
-		<h1>
-			hello world
-		</h1>,
+		<App />,
 		document.getElementById('app'));
 });
 
---
+++ test/test-index.js
@@ -0,0 +1,16 @@
+var React     = require('react');
+var TestUtils = require('react-addons-test-utils');
+var should    = require('chai').should();
+
+var App       = require('../js/components/App');
+
+
+
+describe('App', function() {
+	it('Render hello world', function() {
+
+		var renderer = TestUtils.createRenderer();
+		renderer.render(<App />);
+						var result = renderer.getRenderOutput();
+	});
+});
