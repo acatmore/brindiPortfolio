e400339d07aa147f6bdb6502b56bfbc68c917792
Alex Atmore <acatmore@gmail.com>
2016-12-11 13:37:04 -0800

    create header component

---
+++ js/components/header.jsx
@@ -0,0 +1,26 @@
+var React   = require('react');
+var connect = require('react-redux').connect;
+var actions = require('../actions/index');
+
+
+var Header = React.createClass({
+	render: function() {
+		<header role="banner" className="site-header">
+			<h1>The Header</h1>
+		</header>
+	}
+});
+
+
+
+var mapStateToProps = function(state, props) {
+	return {
+
+	};
+};
+
+
+var Container = connect(mapStateToProps)(Header);
+
+
+module.exports = Container;
