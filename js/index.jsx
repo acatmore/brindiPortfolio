ce970e11011d3f1fcebc8ba2fafaee0fd51d6d97
Alex Atmore <acatmore@gmail.com>
2016-12-16 15:29:23 -0800

    create actions, reducers, store, and render app to page

--- js/index.jsx
+++ js/index.jsx
@@ -1,13 +1,26 @@
 require('babel-polyfill');
-var React      = require('react');
-var ReactDOM   = require('react-dom');
-var Provider   = require('react-redux').Provider;
-var App        = require('./components/App');
+var React      		   = require('react');
+var ReactDOM   		   = require('react-dom');
+var router     		   = require('react-router');
+var Provider   		   = require('react-redux').Provider;
+var store	   		   = require('./store');
+var App        		   = require('./components/App');
+var IllustrationList   = require('./components/illustration-list');
+var Router  		   = require.Router;
+var Route 			   = require.Route;
+var hashHistory        = require.hasHistory;
+var indexRoute 		   = require.IndexRoute;
 
 
 document.addEventListener('DOMContentLoaded', function() {
 	ReactDOM.render(
-		<App />,
+		<Provider store={store}>
+			<Router history={hashHistory}>
+				<Route path="/"  component={App}>
+					<Route path=":slug" component={IllustrationList} />
+				</Route>
+			</Router>
+		</Provider>,
 		document.getElementById('app'));
 });
 
---
+++ js/reducers/index.js
@@ -0,0 +1,40 @@
+var actions = require('../actions/index');
+
+
+var initialState = {
+  name: '',
+  description: '',
+  aboutPage: {},
+  illustrations: [],
+  currentIllustration: {}
+};
+
+
+var appReducer = function(state, action) {
+  state = state || initialState;
+
+  switch (action.type) {
+    case actions.FETCH_ILLUSTRATIONS_SUCCESS :
+      var newIllustrations = state.illustrations.concat([ action.illustrations ]);
+      return Object.assign({}, state, { illustrations: newIllustrations });
+      break;
+
+    case actions.FETCH_SINGLE_ILLUSTRATION_SUCCESS :
+      return Object.assign({}, state, { currentIllustration: action.currentIllustration });
+      break;
+
+    case actions.FETCH_ABOUT_PAGE_SUCCESS :
+      return Object.assign({}, state, { aboutPage: action.page });
+      break;
+
+    case actions.FETCH_SITE_INFO_SUCCESS :
+      return Object.assign({}, state, {
+        name: action.name,
+        description: action.description
+      });
+      break;
+  }
+};
+
+
+exports.appReducer = appReducer;
